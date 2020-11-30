7ad3631b5599f231830231c396fe7ae7c843c073
emilead <71696219+emilead@users.noreply.github.com>
2020-11-30 11:00:54 +0100

    FIrst commit

--- App.js
+++ App.js
@@ -1,14 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+
+import { API_KEY } from './utils/WeatherAPIKey';
+import {weatherInfo} from './utils/WeatherDescriptions';
+
+import weatherDog from './assets/weatherDog.png';
+
+export default class App extends React.Component {
+  state= {
+    temperature: 0,
+    weatherCondition: null,
+    location:null,
+    icon: null,
+    errorMessage:""
+  }
+
+getLocationAsync = async () => {
+  let {status } = await Permissions.askAsync(Permissions.LOCATION);
+  if (status !== 'granted') {
+    this.setState ({
+      errorMessage: 'You do not have permission',
+    });
+  }
+  let location = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.BestForNavigation});
+  const { latitude , longitude } = location.coords
+  this.setState({location: {latitude, longitude}});
+}
+
+fetchWeather(lat, lon) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+  )
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        temperature: json.main.temp,
+        weatherCondition: json.weather[0].main,
+        icon: json.weather[0].icon,
+      });
+    });
+}
+
+componentDidMount(){
+  this.getLocationAsync().then(()=> {
+    this.fetchWeather(this.state.location.latitude,this.state.location.longitude);
+  })
+}
+
+  render() {
+    const {temperature, weatherCondition, icon } = this.state
+    return (
+      <View style={styles.container}>
+        <Image style={{width:130, height:130}} source={{uri: "https://openweathermap.org/img/w/" + icon+".png"}}/>
+        <Text style={styles.title}>PUP Weather Expert</Text>
+        <Text>{ weatherCondition ? 'Today '+ weatherInfo[weatherCondition].title : 'Waiting for weather conditions'}</Text>
+        <Text>{ weatherCondition ? weatherInfo[weatherCondition].subtitle : ''}</Text>
+        <Text>{temperature ? temperature +'°' : ''}</Text>
+        <Image style={{width: 130, height: 130}} source={weatherDog}/>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +70,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  title: {
+    fontSize: 26,
+  }
 });
